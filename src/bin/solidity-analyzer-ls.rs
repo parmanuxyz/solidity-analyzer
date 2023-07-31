add3341c2821b0ed4739beef03a25b97b9a6c640
Meet Mangukiya <meet@flamy.dev>
2023-07-31 09:12:56 +0530

    refactor: async jobs and diagnostics

--- src/bin/solidity-analyzer-ls.rs
+++ src/bin/solidity-analyzer-ls.rs
@@ -1,4 +1,6 @@
-use solidity_analyzer::backend::Backend;
+use std::sync::Arc;
+
+use solidity_analyzer::backend::{Backend, BackendState};
 use tower_lsp::{LspService, Server};
 
 #[tokio::main]
@@ -6,10 +8,13 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend {
-        client,
-        client_capabilities: Default::default(),
-        state: Default::default(),
+    let (service, socket) = LspService::new(|client| {
+        let client_clone = client.clone();
+        Backend {
+            client,
+            client_capabilities: Default::default(),
+            state: Arc::new(BackendState::new(client_clone)),
+        }
     });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
